e5df3f80fa3e3a49059f5caadec611806ff71090
RyanAraujo12-prog <ryan.saraiva.araujo32@gmail.com.br>
2025-05-08 19:07:25 -0300

    adding js and remove tags "tr"

---
+++ main.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" alt="Aprovado"/>';
+const imgReprovado = '<img src="./images/reprovado.png" alt="Reprovado"/>';
+
+let linhas = '';
+// Adiciona uma nova linha na tabela com os dados do formulário
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const inputNomeAtividade =  document.getElementById('nome-atividade');
+    const inputNotaAtividade =  document.getElementById('nota-atividade');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+    // Adiciona a nova linha na tabela
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+})
